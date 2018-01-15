f75a2e5eca2c7fbd900e28fc5927c32cad23512a
michalgorecki <mih.gorecki@gmail.com>
2018-01-15 21:40:23 +0100

    change constant names to uppercase

--- src/AllowedActions.java
+++ src/AllowedActions.java
@@ -3,9 +3,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class AllowedActions {
-    private static final String networkString = "add,delete,modify,clear,hide";
-    private static Set<String> allowedActions = new HashSet<>(Arrays.asList(networkString.split(",")));
-
+    private static final String ACTION_STRING = "add,delete,modify,clear,hide";
+    private static Set<String> allowedActions = new HashSet<>(Arrays.asList(ACTION_STRING.split(",")));
 
     public String and(String s1, String s2){
         return SetUtility.and(s1,s2, allowedActions);
--- src/SocialMediaNetworks.java
+++ src/SocialMediaNetworks.java
@@ -4,8 +4,8 @@ import java.util.Set;
 
 public class SocialMediaNetworks {
 
-    private static final String networkString = "facebook,twitter,youtube,instagram,snapchat";
-    private static Set<String> allowedNetworks = new HashSet<>(Arrays.asList(networkString.split(",")));
+    private static final String NETWORK_STRING = "facebook,twitter,youtube,instagram,snapchat";
+    private static Set<String> allowedNetworks = new HashSet<>(Arrays.asList(NETWORK_STRING.split(",")));
 
     public String and(String s1, String s2){
         return SetUtility.and(s1,s2,allowedNetworks);
